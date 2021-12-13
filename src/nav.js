9a9b0bb7434161dd11dded1122ea9a6c8cebf5e1
jcc98 <juan.ciudad98@gmail.com>
2021-12-13 23:39:59 +0100

    nav changes

--- src/nav.js
+++ src/nav.js
@@ -1,18 +1,18 @@
 import React from 'react'
 import {
-  List, ListItem, ListIcon, OrderedList, UnorderedList, Flex, Spacer
+  List, ListItem, ListIcon, OrderedList, UnorderedList, Flex, Spacer, Text
   } from "@chakra-ui/react"
 
 const Nav = () => {
   return (
-    <Flex>
-      <UnorderedList styleType={"none"}>
-        <ListItem>CV</ListItem>
-        <ListItem>Projects</ListItem>
-        <ListItem>About</ListItem>
-        <ListItem>Contact</ListItem>
+      <UnorderedList color="brand.primary" bg="brand.third" p={2.5} styleType="none">
+        <Flex justifyContent={"space-around"}>
+          <ListItem _hover={{ color: "brand.secondary"}}>Projects</ListItem>
+          <ListItem _hover={{ color: "brand.secondary"}}>About</ListItem>
+          <ListItem _hover={{ color: "brand.secondary"}}>Contact</ListItem>
+          <ListItem _hover={{ color: "brand.secondary"}}>CV</ListItem>
+        </Flex>
       </UnorderedList>
-    </Flex>
   )
 }
 
